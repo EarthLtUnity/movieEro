f980287f7ff8c8e326b00b0fa70e76af0644f4c9
Reinkaos <ds2lvg@gmail.com>
2018-03-23 10:26:56 +0900

    [종민] 멤버 테이블 변경, 인쿠르트단에 jstl 링크 추가

--- src/main/webapp/resources/sql/army_member.sql
+++ src/main/webapp/resources/sql/army_member.sql
@@ -3,14 +3,15 @@ CREATE TABLE MEMBER(
 	MB_ID          VARCHAR2(30),
 	MB_PASSWD      VARCHAR2(30) NOT NULL,
 	MB_NAME        VARCHAR2(30) NOT NULL,
-	MB_MAIL        VARCHAR2(30),
-	MB_MAILDOMAIN  VARCHAR2(20),
+	MB_MAIL        VARCHAR2(30) NOT NULL,
+	MB_MAILDOMAIN  VARCHAR2(20) NOT NULL,
 	MB_PHONE       VARCHAR2(15) NOT NULL,
-	PRIMARY KEY(ID)
+	PRIMARY KEY(MB_ID)
 );
 
-INSERT INTO member values('kjm', 'qwe123', '김종민', 'kjm', 'google.com', '01012341234');
+INSERT INTO member values('admin', 'qwe123', '김종민', 'admin', 'google.com', '01012345678');
+INSERT INTO member values('kjm', '123', '김종민', 'kjm', 'google.com', '01012341234');
 
-SELECT id FROM member WHERE id='km' AND passwd='123qwe';
+UPDATE member SET mb_name='관리자' WHERE mb_id='admin';
 
 SELECT * FROM MEMBER;
