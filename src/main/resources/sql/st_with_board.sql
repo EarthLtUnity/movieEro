10b0de789dcc68a21f447d67163e728b4803831f
exitst8 <exitst8@naver.com>
2018-04-05 12:32:19 +0900

    [승태] sql파일 수정 추가 (st_with_board.sql)

--- src/main/resources/sql/st_with_board.sql
+++ src/main/resources/sql/st_with_board.sql
@@ -1,128 +1,53 @@
+--시퀀스 생성
 drop sequence ST_WITH_BOARD_SEQ;
-
 create sequence ST_WITH_BOARD_SEQ
 start with 1
 increment by 1
 maxvalue 9999
 
 
+--WITH 게시판 
 drop table ST_WITH_BOARD;
 delete ST_WITH_BOARD;
-
 create table ST_WITH_BOARD (	
-	WHID_BOARD_NO NUMBER, /* 게시글번호 */
-	WHID_BOARD_WRITER VARCHAR2(30 BYTE) NOT NULL ENABLE, /* 작성자 */
-	WHID_BOARD_TITLE VARCHAR2(50 BYTE) NOT NULL ENABLE, /* 제목 */
-	WHID_BOARD_CONTENT VARCHAR2(2000 BYTE) NOT NULL ENABLE, /* 내용 */
-	WHID_BOARD_NOW_ID VARCHAR2(400 BYTE) NOT NULL ENABLE, /* 현재 참여자ID */
-	WHID_BOARD_NOW_NO NUMBER, /* 현재참여인원 */
-	WHID_BOARD_APPLY_NO NUMBER, /* 모집인원 */
-	WHID_BOARD_READCOUNT NUMBER, /* 게시글조회수 */
-	WHID_BOARD_DATE DATE DEFAULT sysdate, /* 날시날짜 */
-	primary key (WHID_BOARD_NO));
-	 
-/*초기 값 넣기 10개 */
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL, 
-'admin', 
-'강남 7시 신과함께 보실분', 
-'강남역 /CGV /4명/ 신과함께/ 강남역앞4번출구/ 10:00까지',
-'admin',
-1 ,4 ,1 , sysdate);
+	WITH_BOARD_NO NUMBER, --게시글번호
+	WITH_BOARD_WRITER VARCHAR2(30 BYTE) NOT NULL ENABLE, --작성자 
+	WITH_BOARD_TITLE VARCHAR2(50 BYTE) NOT NULL ENABLE, --제목 
+	WITH_BOARD_CONTENT VARCHAR2(2000 BYTE) NOT NULL ENABLE, -- 내용
+	WITH_BOARD_COUPON VARCHAR2(100 BYTE), --쿠폰함
+	WITH_BOARD_APPLY_NO NUMBER, --모집인원 
+	WITH_BOARD_READCOUNT NUMBER, --게시글조회수 
+	WITH_BOARD_DATE DATE DEFAULT sysdate,  --날시날짜 
+	primary key (WITH_BOARD_NO));
+	
+--WITH_SUB 게시판
+drop table ST_WITH_BOARD_SUB;
+delete ST_WITH_BOARD_SUB;
+create table ST_WITH_BOARD_SUB (	
+	WITH_BOARD_NO NUMBER primary key, --게시글번호 
+	WITH_BOARD_NOW_ID VARCHAR2(400 BYTE) NOT NULL ENABLE, --현재 참여자ID
+	WITH_BOARD_NOW_NO NUMBER, --현재참여인원
+	WITH_BOARD_DATE DATE DEFAULT sysdate, --날시날짜 
+	foreign key (WITH_BOARD_NO) references ST_WITH_BOARD(WITH_BOARD_NO)); --외래키
 
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'steve',
-'목동 7시 다함께차차차 보실분',
-'목동역 /메가박스 /3명 /신과함께 /목동역앞4번출구 /11:00까지',
-'steve,babo',
-2, 3, 1, sysdate);
+	
+--TRIGGER
+--INSERT문 실행시 sub에 insert
+DROP TRIGGER TRG_WITH_BOARD_SUB;
+CREATE OR REPLACE TRIGGER TRG_WITH_BOARD_SUB
+AFTER INSERT
+ON ST_WITH_BOARD
+FOR EACH ROW
+BEGIN
+   INSERT INTO ST_WITH_BOARD_SUB
+   VALUES(:NEW.WITH_BOARD_NO, :NEW.WITH_BOARD_WRITER, 1, SYSDATE);
+END;
+/
 
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'love',
-'이수 4시 어벤져스',
-'이수역 /메가박스 /4명 /어벤져스 /이수역앞4번출구 /21:00까지',
-'love,momo',
-2, 4, 10, sysdate);
+commit
+	
 
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'gogo',
-'삼성 1시 바람과 함께 사라지다 / 4명',
-'삼성역 /CGV /4명 /바람과 함께 사라지다 /삼성역 앞1번출구 /22:00까지',
-'gogo,momo,test,yoo',
-4, 4, 10, sysdate);
 
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'gogo',
-'삼성 1시 바람과 함께 사라지다 / 4명',
-'삼성역 /CGV /4명 /바람과 함께 사라지다 /삼성역 앞1번출구 /22:00까지',
-'gogo,momo,test,yoo',
-4, 4, 10, sysdate);
 
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL, 
-'admin', 
-'강남 7시 신과함께 보실분', 
-'강남역 /CGV /4명/ 신과함께/ 강남역앞4번출구/ 10:00까지',
-'admin',
-1 ,4 ,1 , sysdate);
 
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'steve',
-'목동 7시 다함께차차차 보실분',
-'목동역 /메가박스 /3명 /신과함께 /목동역앞4번출구 /11:00까지',
-'steve,babo',
-2, 3, 1, sysdate);
 
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'love',
-'이수 4시 어벤져스',
-'이수역 /메가박스 /4명 /어벤져스 /이수역앞4번출구 /21:00까지',
-'love,momo',
-2, 4, 10, sysdate);
-
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'gogo',
-'삼성 1시 바람과 함께 사라지다 / 4명',
-'삼성역 /CGV /4명 /바람과 함께 사라지다 /삼성역 앞1번출구 /22:00까지',
-'gogo,momo,test,yoo',
-4, 4, 10, sysdate);
-
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'gogo',
-'삼성 1시 바람과 함께 사라지다 / 4명',
-'삼성역 /CGV /4명 /바람과 함께 사라지다 /삼성역 앞1번출구 /22:00까지',
-'gogo,momo,test,yoo',
-4, 4, 10, sysdate);
-
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL, 
-'admin', 
-'강남 7시 신과함께 보실분', 
-'강남역 /CGV /4명/ 신과함께/ 강남역앞4번출구/ 10:00까지',
-'admin',
-1 ,4 ,1 , sysdate);
-
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'steve',
-'목동 7시 다함께차차차 보실분',
-'목동역 /메가박스 /3명 /신과함께 /목동역앞4번출구 /11:00까지',
-'steve,babo',
-2, 3, 1, sysdate);
-
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'love',
-'이수 4시 어벤져스',
-'이수역 /메가박스 /4명 /어벤져스 /이수역앞4번출구 /21:00까지',
-'love,momo',
-2, 4, 10, sysdate);
-
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'gogo',
-'삼성 1시 바람과 함께 사라지다 / 4명',
-'삼성역 /CGV /4명 /바람과 함께 사라지다 /삼성역 앞1번출구 /22:00까지',
-'gogo,momo,test,yoo',
-4, 4, 10, sysdate);
-
-INSERT INTO ST_WITH_BOARD values (ST_WITH_BOARD_SEQ.NEXTVAL,
-'gogo',
-'삼성 1시 바람과 함께 사라지다 / 4명',
-'삼성역 /CGV /4명 /바람과 함께 사라지다 /삼성역 앞1번출구 /22:00까지',
-'gogo,momo,test,yoo',
-4, 4, 10, sysdate);
